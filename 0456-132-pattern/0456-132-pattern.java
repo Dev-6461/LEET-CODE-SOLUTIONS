f8831d22bcdec3f54add25d3183db22f33e72c6e
Dev-6461 <devrajchaudhary6461@gmail.com>
2026-07-26 23:15:56 +0530

    Time: 13 ms (95.9%), Space: 97.8 MB (88.61%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int third = Integer.MIN_VALUE;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < third) {
+                return true;
+            }
+            while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                third = stack.pop();
+            }
+            stack.push(nums[i]);
+        }
+        return false;
+    }
+}
